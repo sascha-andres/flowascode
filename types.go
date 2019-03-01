bf744ce2be9e2f8023409a458263b0ed73164191
Sascha Andres <sascha.andres@outlook.com>
2019-03-01 16:00:51 +0100

    feat: first take on types

---
+++ types.go
@@ -0,0 +1,24 @@
+package flowascode
+
+type (
+	// Flow represents a complete flow
+	Flow struct {
+		BreakOnError bool   `yaml:"break_on_error"` // BreakOnError breaks if a script returns a non zero result
+		Steps        []Step `yaml:"steps"`          // Steps is the collection of steps in the flow
+	}
+
+	// Step is a single execution point in a flow
+	Step struct {
+		Name      string       // Name of a step, also used to reference from C(Descendant)
+		Script    []string     // Script to execute
+		OnSuccess []Descendant // OnSuccess is a list of steps to execute if the script returns with 0
+		OnFailure []Descendant // OnFailure is a list of steps to execute if the scripts returns not a 0
+	}
+
+	// Descendant represents on step after the current step
+	// that is it is a reference to a number of other steps
+	Descendant struct {
+		Name      string            // Name is the referenced step
+		Variables map[string]string // Variables is a list of variables that are set as environment variables
+	}
+)
